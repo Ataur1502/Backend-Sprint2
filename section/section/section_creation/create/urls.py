86ea2db2dfc2e00f4bae2cc160715eb95eaaaa8d
Victim_Anonymous <134782413+saikarthan@users.noreply.github.com>
2026-01-25 20:58:02 +0530

    Delete section/section/section_creation directory

--- section/section/section_creation/create/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SectionAPIView
-
-urlpatterns = [
-    path('sections/', SectionAPIView.as_view(), name='sections'),               # GET, POST
-    path('sections/<uuid:section_id>/', SectionAPIView.as_view(), name='section-update'),  # PUT
-]
