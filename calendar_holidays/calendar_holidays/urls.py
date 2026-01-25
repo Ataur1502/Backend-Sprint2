2aaaa23b972cb0a790840dda7cafeed3ee957947
Victim_Anonymous <134782413+saikarthan@users.noreply.github.com>
2026-01-25 20:58:15 +0530

    Delete calendar_holidays directory

--- calendar_holidays/calendar_holidays/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CalendarAPIView, HolidayAPIView
-
-urlpatterns = [
-    path('calendars/', CalendarAPIView.as_view()),
-    path('holidays/', HolidayAPIView.as_view()),
-]
